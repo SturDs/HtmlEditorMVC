80f6254e6f9ef07aaebe0a26d38c23c12a653182
Giorgio <brighton2006@googlemail.com>
2019-01-16 10:06:34 +0100

    Content of the HTML Editor
    We have created in this tutorial:
     
    ViewBag.contentOfHTMLEditor = myModel.MyHtmlData;

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 namespace MyRichTextMVC.Controllers
 {
     public class HomeController : Controller
-    {
+    { 
         public ActionResult Index()
         {
             MyModel myModel = new MyModel();
